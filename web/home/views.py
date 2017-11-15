bce0815e5b7dd68b21e622149b4ec09de293aa01
Josh Arnold <jarnld2@gmail.com>
2017-11-15 12:57:24 -0600

    added functionality project init, updated styling, from JSON will update label file structure

--- web/home/views.py
+++ web/home/views.py
@@ -8,16 +8,45 @@ from web import app
 from flask import render_template, redirect, flash, url_for, request, jsonify
 from werkzeug import secure_filename
 
-
-APP_ROOT = path.dirname(path.abspath(__file__))
 ALLOWED_EXTENSIONS = set(['csv', 'xls', 'xlsx'])
-PROJECT_PATH = path.join(APP_ROOT, '../default/')
+PROJECT_PATH = path.join(path.dirname(path.abspath(__file__)), '../default/')
 app.config['PROJECT_PATH'] = PROJECT_PATH
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# files to upload
-#uploaded_csvs = UploadSet('files2upload', csv)
-#configure_uploads(app, uploaded_images)
+def create_default_dir():
+    # create default dir
+    os.mkdir(PROJECT_PATH)
+    # under default create csv dir
+    os.mkdir(os.path.join(PROJECT_PATH, 'csv/'))
+    # under csv create labels dir
+    os.mkdir(os.path.join(PROJECT_PATH, 'csv/labels/'))
+    # under default create images dir
+    os.mkdir(os.path.join(PROJECT_PATH, 'images/'))
+    # under default create models dir
+    os.mkdir(os.path.join(PROJECT_PATH, 'models/'))
+
+def remove_default_dir():
+    shutil.rmtree(PROJECT_PATH)
+
+def remove_and_create_labels_dir():
+    shutil.rmtree(os.path.join(PROJECT_PATH, 'csv/labels/'))
+    os.mkdir(os.path.join(PROJECT_PATH, 'csv/labels/'))
+
+def create_directory(post_data):
+    # for each labels
+    for index in range(len(post_data)):
+        #if label-name folder exists in label folder
+        if os.path.isdir(os.path.join(PROJECT_PATH, 'csv/labels/' + post_data[index]['label'])):
+            # move file from csv folder to label-name folder
+            shutil.move(os.path.join(PROJECT_PATH, 'csv/' + post_data[index]['file_name']), os.path.join(PROJECT_PATH, 'csv/labels/' + post_data[index]['label']))
+        else:
+            # create label-name folder in label folder
+            os.mkdir(os.path.join(PROJECT_PATH, 'csv/labels/' + post_data[index]['label']))
+            # move file in csv folder to new label-name folder
+            shutil.move(os.path.join(PROJECT_PATH, 'csv/' + post_data[index]['file_name']), os.path.join(PROJECT_PATH, 'csv/labels/' + post_data[index]['label']))
 
 @app.route('/')
 @app.route('/home')
@@ -25,10 +54,21 @@ def home():
     # Initialize function from __init__.py
     return render_template("home.html")
 
+@app.route('/initProject', methods=['POST'])
+def initProject():
+    # if default dir exists
+    # delete and create
+    if os.path.isdir(PROJECT_PATH):
+        remove_default_dir();
+        create_default_dir();
+    else:
+        create_default_dir();
+    return (''), 204
+
 @app.route('/loadFiles')
 def loadFiles():
     file_names = []
-    return render_template("loadFiles.html", file_names=file_names)
+    return render_template("loadFiles.html", file_names=file_names, slideUp=True)
 
 @app.route('/transformData')
 def transformData():
@@ -50,10 +90,6 @@ def results():
 def page_not_found(error):
     return render_template('404.html'), 404
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/upload', methods=['POST'])
 def upload():
     file_names = []
@@ -63,14 +99,13 @@ def upload():
     for file in request.files.getlist("file"):
         # if an empty form, return
         if file.filename == '':
-            return render_template('loadFiles.html', file_names=file_names, flag='fail', message="Uh oh, you forgot to add files")
+            return render_template('loadFiles.html', file_names=file_names, flag='fail', message="Uh oh, you forgot to add files", slideUp=False)
         file_names.append(secure_filename(file.filename))
         filename = secure_filename(file.filename)
         file.save(os.path.join(target, filename))
         file.close()
 
-    return render_template('loadFiles.html', file_names=file_names, flag='success', message="Success")
-
+    return render_template('loadFiles.html', file_names=file_names, flag='success', message="Success", slideUp=False)
 
 @app.route('/getjson', methods=['POST'])
 def getjson():
@@ -82,13 +117,7 @@ def getjson():
 
     print( post_data[4]['file_name'] )
 
-    # converts the JSON object into Python recognizable data
-    #req_data = request.get_json()
-    #print(req_data.is_json())
-
-    #fileName_label = req_data['file_name']
-    #print(fileName_label)
-    # print(fileName_label)
+    create_directory(post_data)
 
     # for row in req_data:
     #     print(row['file_name'])
